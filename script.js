009030a8379d11f30339d2ec2767cd1f8852b656
Ryan Hartley <Ryan@elleestphotography.com>
2021-04-21 13:48:45 -0400

    create git repo

---
+++ script.js
@@ -0,0 +1,83 @@
+function initialize() {
+  var myLatLng = new google.maps.LatLng(28.538336, -81.379234);
+  var mapOptions = {
+    center: myLatLng,
+    zoom: 8,
+    disableDefaultUI: true,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+  };
+  var map = new google.maps.Map(
+    document.getElementById("map-canvas"),
+    mapOptions
+  );
+  var contentString =
+    '<div id="content">' +
+    '<div id="sideNotice"></div>' +
+    '<h1 id="firstHeading">Florida</h1>' +
+    '<div id="bodyContent">' +
+    +"</div></div>";
+  var infoWindowOptions = {
+    content: contentString,
+    maxWidth: 400,
+  };
+  var infoWindow = new google.maps.InfoWindow(infoWindowOptions);
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: "Florida",
+  });
+  google.maps.event.addListener(marker, "click", function () {
+    infoWindow.open(map, marker);
+  });
+  google.maps.event.addDomListener(window, "load", initialize);
+}
+
+let sliderImages = document.querySelectorAll(".slide");
+let arrowLeft = document.getElementById("arrow-left");
+
+let arrowRight = document.querySelector("#arrow-right");
+current = 0;
+
+//clear all images
+function reset() {
+  for (let i = 0; i < sliderImages.length; i++) {
+    sliderImages[i].style.display = "none";
+  }
+}
+//init slider
+function startSlide() {
+  reset();
+  sliderImages[0].style.display = "block";
+}
+
+//show previous
+function previous() {
+  reset();
+  sliderImages[current - 1].style.display = "block";
+  current--;
+}
+
+//show next
+function next() {
+  reset();
+  sliderImages[current + 1].style.display = "block";
+  current++;
+}
+
+//left arrow click
+
+arrowLeft.addEventListener("click", function (e) {
+  if (current === 0) {
+    current = sliderImages.length;
+  }
+  previous();
+});
+//left arrow click
+arrowRight.addEventListener("click", function () {
+  if (current === sliderImages.length - 1) {
+    current = -1;
+  }
+  next();
+});
+
+startSlide();
